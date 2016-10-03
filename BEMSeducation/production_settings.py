1f1a7a0cdbea8619b8ae9cb166f17c2445d72911
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-03 23:42:42 +0200

    Settings fix

---
+++ BEMSeducation/production_settings.py
@@ -0,0 +1,24 @@
+from BEMSeducation.common_settings import *
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '130.211.87.224',
+        'NAME': 'bems',
+        'USER': 'root',
+        'PASSWORD': 'beatbcn4113',
+    }
+}
+
+""" Static files and media (CSS, JavaScript, Images) """
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+ENV_PATH = os.path.abspath(os.path.dirname(__file__))
+MEDIA_ROOT = os.path.join(ENV_PATH, '../static_server/media/')
+STATIC_ROOT = os.path.join(ENV_PATH, '../static_server/static/')
+
+# This one is just useful during development, on production statics and media should be served by nginx / apache
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
