d8520bd5e413737a964e6b0ccca312e11004ec68
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-06 23:56:18 +0200

    URL Update

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
-from attendance.views import AttendanceEntryListView, RankingListView
+from attendance.views import AttendanceEntryListView, RankingListView, TodayCallView, CallView
 
 urlpatterns = [
-    url(r'^entry/$', AttendanceEntryListView.as_view()),
-    url(r'^ranking/$', RankingListView.as_view()),
+    url(r'^entry/$', AttendanceEntryListView.as_view(), name='entry-list'),
+    url(r'^ranking/$', RankingListView.as_view(), name='ranking'),
+    url(r'^call/today$', TodayCallView.as_view(), name='call-today'),
+    url(r'^call/(?P<year>[0-9]{4})/(?P<month>[-\w]+)/(?P<day>[0-9]+)/$', CallView.as_view(), name='call'),
 ]
