d001a3f2399e9c54b69c3988b5ddea7999c7fc6e
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-07 17:19:52 +0200

    Error

--- BEMSeducation/urls.py
+++ BEMSeducation/urls.py
@@ -41,14 +41,12 @@ if 'rosetta' in settings.INSTALLED_APPS:
 
 
 def handler404(request):
-    response = render_to_response('special/404.html', {},
-                                  context_instance=RequestContext(request))
+    response = render_to_response('special/404.html')
     response.status_code = 404
     return response
 
 
 def handler500(request):
-    response = render_to_response('special/500.html', {},
-                                  context_instance=RequestContext(request))
+    response = render_to_response('special/500.html')
     response.status_code = 500
     return response
