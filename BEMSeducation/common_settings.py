283ffd81b31fb9d6d746b24abf0c52c75bd50af0
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-21 16:59:24 +0200

    Test

--- BEMSeducation/common_settings.py
+++ BEMSeducation/common_settings.py
@@ -51,7 +51,7 @@ ADDON_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
-    #'debug_toolbar',
+    'debug_toolbar',
     'model_utils',
     'rest_framework',
     'raven.contrib.django.raven_compat',
@@ -82,7 +82,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.admindocs.middleware.XViewMiddleware',
-    #'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'BEMSeducation.urls'
