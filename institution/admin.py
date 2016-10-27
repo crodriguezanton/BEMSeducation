f1812294176587de2c38ac9134f86ee5eacf0fd7
Carlos Rodríguez <c.rodriguezanton@gmail.com>
2016-10-27 11:57:49 +0200

    Institution @ Admin

--- institution/admin.py
+++ institution/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from institution.models import *
 
+admin.site.register(Institution)
 admin.site.register(Stage)
 admin.site.register(Grade)
 admin.site.register(Classroom)
