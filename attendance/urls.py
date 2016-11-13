c40f408e6cbf90a30fe3d53856ba299bae12c15e
Carlos Rodríguez <carlos@beatbcn.com>
2016-11-13 23:14:10 +0100

    URL

--- attendance/urls.py
+++ attendance/urls.py
@@ -6,6 +6,6 @@ from schedule.views import TodayCallView, CallView
 urlpatterns = [
     url(r'^entry/$', AttendanceEntryListView.as_view(), name='entry-list'),
     url(r'^ranking/$', RankingListView.as_view(), name='ranking'),
-    url(r'^call-today/$', TodayCallView.as_view(), name='call-today'),
+    url(r'^call/today/$', TodayCallView.as_view(), name='today'),
     url(r'^call/archive/(?P<year>[0-9]{4})/(?P<month>[0-9]+)/(?P<day>[0-9]+)/$', CallView.as_view(), name='call'),
 ]
