473de7cf4303e1854a5e7208780810fb4ed78102
Carlos Rodríguez <carlos@beatbcn.com>
2016-11-14 01:25:12 +0100

    Attendance

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 
 from attendance.views import AttendanceEntryListView, RankingListView
-from schedule.views import TodayCallView, CallView
+from schedule.views import CallView, add_entry
 
 urlpatterns = [
     url(r'^entry/$', AttendanceEntryListView.as_view(), name='entry-list'),
     url(r'^ranking/$', RankingListView.as_view(), name='ranking'),
     url(r'^call/today/$', CallView.as_view(), name='call-today'),
     url(r'^call/archive/(?P<year>[0-9]{4})/(?P<month>[0-9]+)/(?P<day>[0-9]+)/$', CallView.as_view(), name='call'),
+    url(r'^entry/add/$', add_entry, name='entry-add'),
 ]
--- schedule/views.py
+++ schedule/views.py
@@ -1,9 +1,13 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from datetime import datetime
+
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 from django.views.generic.dates import DayArchiveView, TodayArchiveView, _date_from_string
 
-from education.models import Teacher
+from attendance.models import AttendanceEntry
+from education.models import Teacher, Student
 from schedule.models import TimetableEntry, ClassUnit, ClassDay
 
 
@@ -68,15 +72,25 @@ class CallView(DayArchiveView):
         return super(CallView, self).get_queryset().filter(weekly_timetable_entry__teacher=self.request.profile)
 
 
-class TodayCallView(TodayArchiveView):
-    model = TimetableEntry
-    date_field = 'date'
-    allow_empty = True
+@csrf_exempt
+def add_entry(request):
+    if request.POST.has_key('day'):
 
-    def get_context_data(self, **kwargs):
-        context = super(TodayCallView, self).get_context_data()
-        context['page_aside'] = True
-        return context
+        student = Student.objects.get(pk=request.POST.get('student'))
+        entryquery = AttendanceEntry.objects.filter(student=student, timetable_entry__pk=request.POST.get('entry'))
 
-    def get_queryset(self):
-        return super(TodayCallView, self).get_queryset().filter(weekly_timetable_entry__teacher=self.request.profile)
+        if request.POST.get('status') == 'P':
+            if entryquery.count() != 0:
+                entryquery.delete()
+        else:
+            if entryquery.count() != 0:
+                entry = entryquery[0]
+                entry.type = request.POST.get('status')
+            else:
+                entry = AttendanceEntry(student=student, timetable_entry = TimetableEntry.objects.get(pk=request.POST.get('entry')), type=request.POST.get('status'))
+
+            entry.save()
+
+        return JsonResponse({'success': True, 'entryquery': entryquery.count()})
+
+    return JsonResponse({'success': False})
