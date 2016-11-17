dca776c9dceabc572f14c6b0d0b58769b29fee48
Carlos Rodríguez <carlos@beatbcn.com>
2016-11-17 01:48:29 +0100

    Added Create Incident URL

--- incidents/views.py
+++ incidents/views.py
@@ -11,6 +11,7 @@ from incidents.models import Incident
 class IncidentCreateView(CreateView):
     model = Incident
     form_class = IncidentForm
+    success_url = '/'
 
     def get_context_data(self, **kwargs):
         context = super(IncidentCreateView, self).get_context_data(**kwargs)
