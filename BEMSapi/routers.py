a857b1fe8c148e865ee6b7d72f7ed2d1f6d16867
Carlos Rodríguez <carlos@beatbcn.com>
2017-05-09 11:19:09 +0200

    basename

--- BEMSapi/routers.py
+++ BEMSapi/routers.py
@@ -2,4 +2,4 @@ from rest_framework import routers
 from BEMSapi.viewsets import TimetableEntryViewSet
 
 default_router = routers.DefaultRouter()
-default_router.register(r'timetable_entry', TimetableEntryViewSet)
+default_router.register(r'timetable_entry', TimetableEntryViewSet, base_name='timetable_entry-detail')
