0985bf7d8a006c7f6314ed24fed550c9778adc88
Carlos Rodríguez <carlos@beatbcn.com>
2017-05-09 11:16:32 +0200

    queryset

--- BEMSapi/viewsets.py
+++ BEMSapi/viewsets.py
@@ -1,3 +1,4 @@
+import datetime
 from rest_framework import permissions
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
@@ -11,6 +12,11 @@ class StandardResultsSetPagination(PageNumberPagination):
     max_page_size = 1000
 
 class TimetableEntryViewSet(viewsets.ModelViewSet):
-    queryset = TimetableEntry.objects.filter(weekly_timetable_entry__teacher__pk="d1864d7e-b772-4aae-bb30-cd02a1a275c8")
     serializer_class = TimetableEntrySerializer
     pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        te = TimetableEntry.objects.filter(weekly_timetable_entry__teacher__pk="d1864d7e-b772-4aae-bb30-cd02a1a275c8")
+        if self.request.GET.get('start', '') != '':
+            te = te.filter(date__gte=datetime.datetime.fromtimestamp(int(self.request.GET.get('start', ''))), date__lte=datetime.datetime.fromtimestamp(int(self.request.GET.get('end', ''))))
+        return te
