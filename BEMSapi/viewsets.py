87925967fef1291fb663f6e504dd78dccb2a00a9
Carlos Rodríguez <carlos@beatbcn.com>
2017-05-09 10:20:42 +0200

    not paginate

--- BEMSapi/viewsets.py
+++ BEMSapi/viewsets.py
@@ -13,4 +13,4 @@ class StandardResultsSetPagination(PageNumberPagination):
 class TimetableEntryViewSet(viewsets.ModelViewSet):
     queryset = TimetableEntry.objects.filter(weekly_timetable_entry__teacher__pk="d1864d7e-b772-4aae-bb30-cd02a1a275c8")
     serializer_class = TimetableEntrySerializer
-    pagination_class = StandardResultsSetPagination
+#    pagination_class = StandardResultsSetPagination
